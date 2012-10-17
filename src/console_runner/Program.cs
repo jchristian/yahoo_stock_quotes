7bda7862fb96bc1dd3950924f3a9a96abb4729bf
Jonathan Christian <jont.christian@gmail.com>
2012-10-17 16:51:36 -0700

    Added a console runner.

---
+++ src/console_runner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using YSQ.core;
+
+namespace YSQ.console_runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quote_service = new QuoteService();
+            var quotes = quote_service.Quote("MSFT", "GOOG").Return(QuoteReturnParameter.Symbol,
+                                                                    QuoteReturnParameter.Name,
+                                                                    QuoteReturnParameter.LatestPrice,
+                                                                    QuoteReturnParameter.LatestTime);
+
+            foreach (var quote in quotes)
+            {
+                Console.WriteLine("{0} - {1} - {2} - {3}", quote.Symbol, quote.Name, quote.LatestPrice, quote.LatestTime);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
